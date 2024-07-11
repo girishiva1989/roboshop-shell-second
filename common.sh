2109a72766380fe18b81e8fa47d9258e2765e9eb
girish <girishiva1989@gmail.com>
2024-07-12 09:54:02 +1000

    Add Java function

--- common.sh
+++ common.sh
@@ -10,18 +10,27 @@ fun_schema_setup()
 {
   if [ "$schema_setup" == "mongod" ]
   then
-  fun_print_head "setup MongoDB repo"
-  cp ${script_path}/mongod.repo /etc/yum.repos.d/mongo.repo
+   fun_print_head "setup MongoDB repo"
+   cp ${script_path}/mongod.repo /etc/yum.repos.d/mongo.repo
 
-  fun_print_head "Install mongodb-client"
-  dnf install mongodb-org-shell -y
+   fun_print_head "Install mongodb-client"
+   dnf install mongodb-org-shell -y
 
-  fun_print_head "load the schema"
-  mongo --host mongod-dev.gdevops89.online </app/schema/${component}.js
+   fun_print_head "load the schema"
+   mongo --host mongod-dev.gdevops89.online </app/schema/${component}.js
 
-  fun_print_head "Restart Service"
-  systemctl restart ${component}
-  fi
+   fun_print_head "Restart Service"
+   systemctl restart ${component}
+   fi
+
+   if [ "$schema_setup" == "mysql" ]
+   then
+    fun_print_head "Install mysql client"
+    dnf install mysql -y
+
+    fun_print_head "Load schema"
+    mysql -h mysql-dev.gdevops89.online -uroot -p${mysql_user_password}< /app/schema/${component.sql
+   fi
 }
 fun_nodejs()
 {
@@ -57,4 +66,39 @@ fun_nodejs()
 
   fun_schema_setup
 
+}
+
+fun_java()
+{
+  fun_print_head "Install Maven"
+  dnf install maven -y
+
+  fun_print_head "Add application User"
+  useradd ${app_user}
+
+  fun_print_head "Setup app directory"
+  rm -rf /app
+  mkdir /app
+
+  fun_print_head "Download the application code"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+  unzip /tmp/${component}.zip
+
+  fun_print_head "Download the dependencies"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+  fun_print_head "Setup SystemD Shipping Service"
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+  fun_print_head "Load and Start the service"
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl start ${component}
+
+  fun_schema_setup
+
+  echo -e "\e[31m<<<<<<<<<Restart Service>>>>>>>>>\e[0m"
+  systemctl restart ${component}
 }
--- shipping.sh
+++ shipping.sh
@@ -2,6 +2,8 @@ script=$(realpath $0)
 script_path=$(dirname $script)
 source ${script_path}/common.sh
 #RoboShop@1
+component=shipping
+schema_setup=mysql
 mysql_user_password=$*
 
 if  [ -z "${mysql_user_password}" ]
@@ -10,38 +12,5 @@ then
   exit
 fi
 
-echo -e "\e[31m<<<<<<<<<Install Maven>>>>>>>>>\e[0m"
-dnf install maven -y
+fun_java
 
-echo -e "\e[31m<<<<<<<<<Add application User>>>>>>>>>\e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m<<<<<<<<<Setup app directory>>>>>>>>>\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m<<<<<<<<<Download the application code>>>>>>>>>\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-
-echo -e "\e[31m<<<<<<<<<Download the dependencies>>>>>>>>>\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[31m<<<<<<<<<Setup SystemD Shipping Service>>>>>>>>>\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>>\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
-
-echo -e "\e[31m<<<<<<<<<Install mysql client>>>>>>>>>\e[0m"
-dnf install mysql -y
-
-echo -e "\e[31m<<<<<<<<<Load schema>>>>>>>>>\e[0m"
-mysql -h mysql-dev.gdevops89.online -uroot -p${mysql_user_password}< /app/schema/shipping.sql
-
-echo -e "\e[31m<<<<<<<<<Restart Service>>>>>>>>>\e[0m"
-systemctl restart shipping
