ecb4c540273b0ba1cd1a62821ef5559369f8951a
girish <girishiva1989@gmail.com>
2024-07-24 22:30:07 +1000

    Add function

--- redis.sh
+++ redis.sh
@@ -19,3 +19,7 @@ sed -i -e 's|127.0.0.1|0.0.0.0|g' /etc/redis.conf /etc/redis/redis.conf &>>${log
 fun_status_check $?
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
 
+func_print_head "Start Redis Service"
+systemctl enable redis &>>$log_file
+systemctl restart redis &>>$log_file
+func_stat_check $?
